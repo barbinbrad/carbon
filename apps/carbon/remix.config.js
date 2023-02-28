726e8308826a69b9fa288ef7f8ef2376b4e35264
Brad Barbin <bbarbin@teamworks.com>
2023-02-28 12:49:13 -0500

    update flatroutes remix config

--- apps/carbon/remix.config.js
+++ apps/carbon/remix.config.js
@@ -11,7 +11,7 @@ module.exports = {
   assetsBuildDirectory: "public/build",
   routes: async (defineRoutes) => {
     return flatRoutes("routes", defineRoutes, {
-      appDir: path.resolve(path.dirname(""), "app"),
+      appDir: path.resolve(__dirname, "app"),
     });
   },
   serverDependenciesToBundle: [

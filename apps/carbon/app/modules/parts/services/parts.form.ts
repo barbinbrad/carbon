3377fd3603ad4ae4fcebe99acfb5418bed95eaae
Brad Barbin <bbarbin@teamworks.com>
2023-04-03 22:43:30 -0400

    parts form wip

--- apps/carbon/app/modules/parts/services/parts.form.ts
+++ apps/carbon/app/modules/parts/services/parts.form.ts
@@ -5,11 +5,15 @@ import { zfd } from "zod-form-data";
 export const partValidator = withZod(
   z.object({
     id: z.string().min(1, { message: "Part ID is required" }).max(255),
-    name: zfd.text(z.string().min(1).max(255)),
+    name: z.string().min(1, { message: "Name is required" }).max(255),
     description: zfd.text(z.string().optional()),
     blocked: zfd.checkbox(),
     partGroupId: z.string().min(20, { message: "Part Group is required" }),
-    partType: z.enum(["Inventory", "Non-Inventory", "Service"]),
+    partType: z.enum(["Inventory", "Non-Inventory", "Service"], {
+      errorMap: (issue, ctx) => ({
+        message: "Part type is required",
+      }),
+    }),
     manufacturerPartNumber: zfd.text(z.string().optional()),
     unitOfMeasureCode: z
       .string()
@@ -20,7 +24,11 @@ export const partValidator = withZod(
 export const partCostValidator = withZod(
   z.object({
     partId: z.string().min(1, { message: "Part ID is required" }),
-    costingMethod: z.enum(["Standard", "Average", "FIFO", "LIFO"]),
+    costingMethod: z.enum(["Standard", "Average", "FIFO", "LIFO"], {
+      errorMap: (issue, ctx) => ({
+        message: "Costing method is required",
+      }),
+    }),
     standardCost: zfd.numeric(z.number().min(0)),
     unitCost: zfd.numeric(z.number().min(0)),
     costIsAdjusted: zfd.checkbox(),
@@ -39,14 +47,17 @@ export const partUnitSalePriceValidator = withZod(
   })
 );
 
-export const partReplenishmentValidator = withZod(
+export const partPurchasingValidator = withZod(
   z.object({
     partId: z.string().min(1, { message: "Part ID is required" }),
-    replenishmentSystem: z.enum([
-      "Purchased",
-      "Manufactured",
-      "Purchased and Manufactured",
-    ]),
+    replenishmentSystem: z.enum(
+      ["Purchased", "Manufactured", "Purchased and Manufactured"],
+      {
+        errorMap: (issue, ctx) => ({
+          message: "Replenishment system is required",
+        }),
+      }
+    ),
     leadTime: zfd.numeric(z.number().min(0)),
     supplierId: zfd.text(z.string().optional()),
     supplierPartNumber: zfd.text(z.string().optional()),
@@ -54,7 +65,11 @@ export const partReplenishmentValidator = withZod(
       .string()
       .min(1, { message: "Unit of Measure is required" }),
     purchasingBlocked: zfd.checkbox(),
-    manufacturingPolicy: z.enum(["Make to Order", "Make to Stock"]),
+    manufacturingPolicy: z.enum(["Make to Order", "Make to Stock"], {
+      errorMap: (issue, ctx) => ({
+        message: "Manufacturing policy is required",
+      }),
+    }),
     scrapPercentage: zfd.numeric(z.number().min(0).max(100)),
     lotSize: zfd.numeric(z.number().min(0)),
   })

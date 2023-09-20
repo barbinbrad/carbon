6dd23c14208a95e3be289d9d87ee4fc4c3c80ae2
Brad Barbin <bbarbin@teamworks.com>
2023-09-19 21:58:33 -0400

    feat: add realtime to data provider

--- apps/carbon/app/stores/data.tsx
+++ apps/carbon/app/stores/data.tsx
@@ -1,7 +1,8 @@
-import { Flex } from "@chakra-ui/react";
+import { Image, Text, useColorModeValue, VStack } from "@chakra-ui/react";
 import { useStore } from "@nanostores/react";
 import { atom, computed } from "nanostores";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { Background } from "~/components/Layout";
 import { useSupabase } from "~/lib/supabase";
 import type { ListItem } from "~/types";
 import { useValue } from "./nanostore";
@@ -39,7 +40,7 @@ export const RealtimeDataProvider = ({
   const [, setSuppliers] = useSuppliers();
   const [, setCustomers] = useCustomers();
 
-  const fetchData = useCallback(async () => {
+  const fetchData = async () => {
     if (!supabase || !accessToken) return;
 
     const [parts, suppliers, customers] = await Promise.all([
@@ -60,12 +61,117 @@ export const RealtimeDataProvider = ({
     setCustomers(customers.data ?? []);
 
     setLoading(false);
-  }, [supabase, accessToken, setParts, setSuppliers, setCustomers]);
+  };
 
   useEffect(() => {
     if (!supabase || !accessToken) return;
     fetchData();
-  }, [supabase, accessToken, fetchData]);
+
+    supabase.realtime.setAuth(accessToken);
+    const channel = supabase
+      .channel("realtime:core")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "part",
+        },
+        (payload) => {
+          const { new: inserted } = payload;
+          setParts((parts) => [
+            ...parts,
+            {
+              id: inserted.id,
+              name: inserted.name,
+              replenishmentSystem: inserted.replenishmentSystem,
+            },
+          ]);
+        }
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "DELETE",
+          schema: "public",
+          table: "part",
+        },
+        (payload) => {
+          const { old: deleted } = payload;
+          setParts((parts) => parts.filter((p) => p.id !== deleted.id));
+        }
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "customer",
+        },
+        (payload) => {
+          const { new: inserted } = payload;
+          setCustomers((customers) => [
+            ...customers,
+            {
+              id: inserted.id,
+              name: inserted.name,
+            },
+          ]);
+        }
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "DELETE",
+          schema: "public",
+          table: "part",
+        },
+        (payload) => {
+          const { old: deleted } = payload;
+          setCustomers((customers) =>
+            customers.filter((c) => c.id !== deleted.id)
+          );
+        }
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "supplier",
+        },
+        (payload) => {
+          const { new: inserted } = payload;
+          setSuppliers((suppliers) => [
+            ...suppliers,
+            {
+              id: inserted.id,
+              name: inserted.name,
+            },
+          ]);
+        }
+      )
+      .on(
+        "postgres_changes",
+        {
+          event: "DELETE",
+          schema: "public",
+          table: "part",
+        },
+        (payload) => {
+          const { old: deleted } = payload;
+          setSuppliers((suppliers) =>
+            suppliers.filter((c) => c.id !== deleted.id)
+          );
+        }
+      )
+      .subscribe();
+
+    return () => {
+      if (channel) supabase?.removeChannel(channel);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [supabase, accessToken]);
 
   if (loading) {
     return <Loading />;
@@ -76,15 +182,26 @@ export const RealtimeDataProvider = ({
 
 function Loading() {
   return (
-    <Flex
-      bg="white"
+    <VStack
       h="100vh"
-      w="full"
+      w="100vw"
       alignItems="center"
       justifyContent="center"
-      zIndex="50"
+      spacing={4}
     >
-      Loading...
-    </Flex>
+      <Image
+        src={useColorModeValue(
+          "/carbon-logo-dark.png",
+          "/carbon-logo-light.png"
+        )}
+        alt="Carbon Logo"
+        maxW={100}
+        marginBottom={3}
+      />
+      <Text letterSpacing={-1} textTransform="uppercase" fontWeight={700}>
+        Loading...
+      </Text>
+      <Background />
+    </VStack>
   );
 }

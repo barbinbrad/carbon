d7e51a306c6129a697301b525453ab466e7b859c
crudmachine <162540227+crudmachine@users.noreply.github.com>
2024-03-07 03:34:30 -0500

    getTableSchema and getViewSchema

---
+++ apps/carbon/app/modules/shared/shared.server.ts
@@ -0,0 +1,49 @@
+import { SUPABASE_API_URL, SUPABASE_STUDIO_URL } from "~/config/env";
+import type { Database } from "@carbon/database";
+import type { SupabaseClient } from "@supabase/supabase-js";
+
+export async function getTableSchema(
+  client: SupabaseClient<Database>,
+  tableId: string
+) {
+  const accessToken = await client.realtime.accessToken;
+  if (!accessToken) throw Error("Failed to get accessToken");
+  const projectRef = ""
+  const supabaseProjectDetailsEndpoint = SUPABASE_API_URL.includes("localhost") ? 
+    `${SUPABASE_STUDIO_URL}/api/projects/default` : 
+      `${SUPABASE_API_URL}/platform/projects/${projectRef}`;
+  const supabaseProject = await fetch(`${supabaseProjectDetailsEndpoint}`);
+  const data = await supabaseProject.json();
+  if (!data) throw Error("Failed to fetch connection string");
+  // platform/projects/{project_ref_here} with bearer token and project ref to get connectionString
+  const connectionString = data.connectionString;
+  const tableSchemaEndpoint = SUPABASE_API_URL.includes("localhost") ? 
+    `${SUPABASE_STUDIO_URL}/api/pg-meta/default/tables?id=${tableId}` : 
+      `${SUPABASE_API_URL}/platform/pg-meta/{project_ref_here}/tables?id=${tableId}`;
+  const tableSchemaResponse = await fetch(`${tableSchemaEndpoint}`);
+  const response = tableSchemaResponse.json();
+  return response;
+}
+
+export async function getViewSchema(
+  client: SupabaseClient<Database>,
+  viewId: string
+) {
+  const accessToken = await client.realtime.accessToken;
+  if (!accessToken) throw Error("Failed to get accessToken");
+  const projectRef = ""
+  const supabaseProjectDetailsEndpoint = SUPABASE_API_URL.includes("localhost") ? 
+    `${SUPABASE_STUDIO_URL}/api/projects/default` : 
+      `${SUPABASE_API_URL}/platform/projects/${projectRef}`;
+  const supabaseProject = await fetch(`${supabaseProjectDetailsEndpoint}`);
+  const data = await supabaseProject.json();
+  if (!data) throw Error("Failed to fetch connection string");
+  // platform/projects/{project_ref_here} with bearer token and project ref to get connectionString
+  const connectionString = data.connectionString;
+  const viewSchemaEndpoint = SUPABASE_API_URL.includes("localhost") ? 
+    `${SUPABASE_STUDIO_URL}/api/pg-meta/default/views?id=${viewId}` : 
+      `${SUPABASE_API_URL}/platform/pg-meta/{project_ref_here}/views?id=${viewId}`;
+  const viewSchemaResponse = await fetch(`${viewSchemaEndpoint}`);
+  const response = viewSchemaResponse.json();
+  return response;
+}
